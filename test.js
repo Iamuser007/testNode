0913774fe7896c48e0d787f844c4129e1d224910
Jasper LO <mufasaleostein@gmail.com>
2023-10-26 00:34:01 +0100

    hfhfhf

---
+++ test.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');	
+const chai = require('chai');
+const server = require('./server');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+ describe('/GET', () => {
+      it('should get the homepage', (done) => {
+        chai.request(server)
+            .get('/');
+              done();
+      });
+  });
