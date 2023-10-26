d8ea3238f2a7142b61b082da24167c35bc101e8a
Jasper LO <mufasaleostein@gmail.com>
2023-10-26 04:37:01 +0100

    mmm

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-// const mocha = require("mocha");
-// const chai = require("chai");
-// const server = require("./server");
-// const chaiHttp = require("chai-http");
+const mocha = require("mocha");
+const chai = require("chai");
+const server = require("./server");
+const chaiHttp = require("chai-http");
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe("/GET", () => {
-//   it("should get the homepage", (done) => {
-//     chai.request(server).get("/");
-//     done();
-//   });
-// });
-
-test("1 + 1 = 2", () => {
-  expect(1 + 1).toBe(2);
+describe("/GET", () => {
+  it("should get the homepage", (done) => {
+    chai.request(server).get("/");
+    done();
+  });
 });
